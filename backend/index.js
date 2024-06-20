45bb09b3028e93251d277669a9c66d2f4886c146
Etsubdink Yetwale <etsubie2124@gmail.com>
2024-06-20 11:14:41 +0300

    updated

--- backend/index.js
+++ backend/index.js
@@ -4,35 +4,33 @@ import dotenv from "dotenv";
 import cors from "cors";
 import songRouter from "./routes/songRoute.js";
 
-
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 4001; 
 const MongoDBURL = process.env.CONNECTION_URL;
 
-//middleware
+// Middleware
 app.use(express.json({ limit: '10mb' })); 
 app.use(cors({
   origin: '*'
 }));
 
-
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
   console.log(req)
- res.json('welcome')
-})
+  res.json('welcome')
+});
 
-app.use('/songs',songRouter)
+app.use('/songs', songRouter);
 
 mongoose
   .connect(MongoDBURL)
   .then(() => {
-    console.log("mongoDB database is connected successfuly!");
+    console.log("MongoDB database is connected successfully!");
     app.listen(port, () => {
-      console.log(`server is running on port: ${port}`);
+      console.log(`Server is running on port: ${port}`);
     });
   })
   .catch((error) => {
-    console.log({message: error.message});
+    console.log({ message: error.message });
   });
