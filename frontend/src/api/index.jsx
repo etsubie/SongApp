969f01e5430d6a1f3b22db21bad2dc2fd896f992
Etsubdink Yetwale <etsubie2124@gmail.com>
2024-06-23 11:18:14 +0300

    updated

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://songapp-g01e.onrender.com' });
+const API = axios.create({ baseURL: 'https://songapp-g01e.onrender.com/' });
 
 export const getSongsAPI = async () => API.get('/all');
 export const getSongByIdAPI = async (id) => API.get(`/${id}`);
