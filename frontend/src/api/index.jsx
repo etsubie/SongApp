b9fdba8528d40e39f46583aadb090c648ffc80bf
Etsubdink Yetwale <etsubie2124@gmail.com>
2024-06-24 09:55:14 +0300

    updated

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://songapp-g01e.onrender.com/songs' });
+const API = axios.create({ baseURL: 'https://songapp-g01e.onrender.com/' });
 
-export const getSongsAPI = async () => API.get('/all');
-export const getSongByIdAPI = async (id) => API.get(`/${id}`);
-export const createSongAPI = async (song) => API.post('/create', song);
-export const updateSongAPI = async (song, id) => API.patch(`/${id}`, song);
-export const deleteSongByIdAPI = async (id) => API.delete(`/${id}`);
+export const getSongsAPI = async () => API.get('/songs/all');
+export const getSongByIdAPI = async (id) => API.get(`/songs/${id}`);
+export const createSongAPI = async (song) => API.post('/songs/create', song);
+export const updateSongAPI = async (song, id) => API.patch(`/songs/${id}`, song);
+export const deleteSongByIdAPI = async (id) => API.delete(`/songs/${id}`);
